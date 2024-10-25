a9ce901166bd0ece75b616aa7e07e10fb18d4b3e
Esteban Salas <juan.salasf@udea.edu.co>
2024-10-24 19:21:18 -0500

    feat: agregar time.c

---
+++ ejercicios_refuerzo/time.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <command>\n", argv[0]);
+        return 1;
+    }
+
+    struct timeval start, end;
+    pid_t pid;
+    gettimeofday(&start, NULL);
+
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0) {
+        execvp(argv[1], &argv[1]);
+        perror("execvp");
+        exit(1);
+    } else {
+        wait(NULL);
+        gettimeofday(&end, NULL);
+        double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+        printf("Elapsed time: %.5f seconds\n", elapsed);
+    }
+    return 0;
+}
